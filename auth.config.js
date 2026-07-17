352596419da2f4132738efde88ebd857cf987c4e
SailuNeverDies <shaileshpratihast85@gmail.com>
2026-07-17 16:47:31 +0530

    Fix middleware Edge size limit by splitting NextAuth config

---
+++ auth.config.js
@@ -0,0 +1,28 @@
+export default {
+  providers: [], // Basic providers (Credentials will be added in lib/auth.js)
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/auth/signin',
+    error: '/auth/signin',
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+  secret: process.env.AUTH_SECRET,
+};
